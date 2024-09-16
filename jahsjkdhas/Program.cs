456f5cc1e3a6942699d7acf0b0f80b0e99ffcd39
SPerez2001 <s.perez.501627@gmail.com>
2024-09-16 20:18:17 +0800

    Add project files.

---
+++ jahsjkdhas/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using jahsjkdhas;
+
+Console.WriteLine(sdasd.str1);
---
+++ jahsjkdhas/sdasd.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jahsjkdhas
+{
+    internal class sdasd
+    {
+        public static string str1 = "Stepped on Me? Stepped on Me! Are you kidding?";
+        public static string str2 = "The Guy was dancing on me, I mean just look at this. Broken. Broken";
+        public static string str3 = "Gone. Gone. Broken. Broken. Broken. - Sea Urchin from Surfs Up(2007)";
+    }
+}
